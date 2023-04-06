1e242f51749e31eee51423271ebb621d71376f94
Muhammad Haikal A <haikal20001@outlook.com>
2023-04-06 22:32:35 +0700

    Configure nvs, add alias for nvim and lazygit, add custom function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,13 @@
 $CONFIGDIR = "(Get-Item $PROFILE).Directory.fullname"
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/blueish.omp.json" | Invoke-Expression
+
+Import-Module 'C:\Users\mhaikala\AppData\Local\vcpkg\scripts\posh-vcpkg'
+nvs use lts > NUL
+
+Set-Alias -Name lg -Value lazygit
+Set-Alias -Name vim -Value nvim
+
+function Get-IP
+{
+  Get-NetIPAddress | Where-Object -Property AddressFamily -eq IPv4 | Select-Object -Property IPAddress
+}
